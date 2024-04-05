305974d4d7e608f13322534826c927463618f2bf
ishaanv1206 <142810253+ishaanv1206@users.noreply.github.com>
2024-04-06 03:58:32 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -140,58 +140,58 @@ if nav=='Predictions':
         st.write("You must wait for few seconds for your prediction")
         if submit:
             import numpy as np
-        import matplotlib.pyplot as plt
-        import pandas as pd
+            import matplotlib.pyplot as plt
+            import pandas as pd
 
-        # Importing the dataset
-        dataset = pd.read_csv('Heart_Disease_Prediction.csv')
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
-        from sklearn.preprocessing import LabelEncoder
-        le = LabelEncoder()
-        y = le.fit_transform(y)
-
-
-        #   Splitting the dataset into the Training set and Test set
-        from sklearn.model_selection import train_test_split
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+            # Importing the dataset
+            dataset = pd.read_csv('Heart_Disease_Prediction.csv')
+            X = dataset.iloc[:, :-1].values
+            y = dataset.iloc[:, -1].values
+            from sklearn.preprocessing import LabelEncoder
+            le = LabelEncoder()
+            y = le.fit_transform(y)
 
 
-        # Feature Scaling
-        from sklearn.preprocessing import StandardScaler
-        sc = StandardScaler()
-        X_train = sc.fit_transform(X_train)
-        X_test = sc.transform(X_test)
-        print(X_train)
-        print(X_test)
+            #   Splitting the dataset into the Training set and Test set
+            from sklearn.model_selection import train_test_split
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-        # Training the Random Forest Classification model on the Training set
-        from sklearn.ensemble import RandomForestClassifier
-        classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
-        classifier.fit(X_train, y_train)
 
-        # Predicting a new result
+            # Feature Scaling
+            from sklearn.preprocessing import StandardScaler
+            sc = StandardScaler()
+            X_train = sc.fit_transform(X_train)
+            X_test = sc.transform(X_test)
+            print(X_train)
+            print(X_test)
 
-        # Predicting the Test set results
-        y_pred = classifier.predict(X_test)
+            # Training the Random Forest Classification model on the Training set
+            from sklearn.ensemble import RandomForestClassifier
+            classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+            classifier.fit(X_train, y_train)
+
+            # Predicting a new result
+
+            # Predicting the Test set results
+            y_pred = classifier.predict(X_test)
         
 
-        # Making the Confusion Matrix
-        from sklearn.metrics import confusion_matrix, accuracy_score
-        cm = confusion_matrix(y_test, y_pred)
-        acc = accuracy_score(y_test, y_pred)
-        new_data = [[sexvar, agevar, chestpainvar, bpvar, cholestrolvar, fbsoveronevar, ecgvar, exerciseanginavar, maxhrvar, stdepressionvar, slopeofstvar, noofvesselsflurovar, thalliumvar]]
-        result = classifier.predict(sc.transform(new_data))
-        if result==[0]:
-            st.markdown(":red[***You have chances of having heart disease***]")
-            st.markdown(":orange[You must always consult a doctor regarding your health. Do not take this tool as a substitute to medical proffessional]")
-            st.write("Accuray of prediction is : ")
-            st.write(acc*100)
-        elif result==[1]:
-            st.markdown(":green[***You dont have chances of having heart disease***]")
-            st.markdown(":orange[You must always consult a doctor regarding your health. Do not take this tool as a substitute to medical proffessional]")
-            st.write("Accuray of prediction is : ")
-            st.write(acc*100)
+            # Making the Confusion Matrix
+            from sklearn.metrics import confusion_matrix, accuracy_score
+            cm = confusion_matrix(y_test, y_pred)
+            acc = accuracy_score(y_test, y_pred)
+            new_data = [[sexvar, agevar, chestpainvar, bpvar, cholestrolvar, fbsoveronevar, ecgvar, exerciseanginavar, maxhrvar, stdepressionvar, slopeofstvar, noofvesselsflurovar, thalliumvar]]
+            result = classifier.predict(sc.transform(new_data))
+            if result==[0]:
+                st.markdown(":red[***You have chances of having heart disease***]")
+                st.markdown(":orange[You must always consult a doctor regarding your health. Do not take this tool as a substitute to medical proffessional]")
+                st.write("Accuray of prediction is : ")
+                st.write(acc*100)
+            elif result==[1]:
+                st.markdown(":green[***You dont have chances of having heart disease***]")
+                st.markdown(":orange[You must always consult a doctor regarding your health. Do not take this tool as a substitute to medical proffessional]")
+                st.write("Accuray of prediction is : ")
+                st.write(acc*100)
 
     elif option=="Human Stress Detection based on physiological data":
 
@@ -347,7 +347,7 @@ if nav=='Predictions':
 
 
             #Predicting a new result
-            result  = classifier.predict(sc.transform([[16.87, 85.87, 50]]))
+            
 
             result = classifier.predict(sc.transform([[snoringratevar, respirationratevar, bodytempvar, limbmovementvar, bloodoxygenvar, eyemovementvar, sleepinghoursvar, heartratevarvar]]))
             if result == [0]:
